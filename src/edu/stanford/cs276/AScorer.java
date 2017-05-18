ef4461ef3a948428e36184985167147b493af871
Pratyaksh Sharma <pratyaksh@me.com>
2017-05-17 21:16:52 -0700

    decode percent encoding for urls

--- src/edu/stanford/cs276/AScorer.java
+++ src/edu/stanford/cs276/AScorer.java
@@ -1,23 +1,22 @@
 package edu.stanford.cs276;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
- * An abstract class for a scorer. 
+ * An abstract class for a scorer.
  * Needs to be extended by each specific implementation of scorers.
  */
 public abstract class AScorer {
 
   // Map: term -> idf
-  Map<String,Double> idfs; 
+  Map<String,Double> idfs;
 
   // Various types of term frequencies that you will need
   String[] TFTYPES = {"url","title","body","header","anchor"};
-  
+
   /**
    * Construct an abstract scorer with a map of idfs.
    * @param idfs the map of idf scores
@@ -35,25 +34,25 @@ public abstract class AScorer {
   {
     return "Pagerank: " + Integer.toString(d.page_rank);
   }
-  
+
     /**
      * Score each document for each query.
      * @param d the Document
      * @param q the Query
      */
   public abstract double getSimScore(Document d, Query q);
-  
+
   /**
    * Get frequencies for a query.
    * @param q the query to compute frequencies for
    */
-  
+
   //Doing sublinear scaling for freq
-  
+
   public Map<String,Double> getQueryFreqs(Query q) {
 
     // queryWord -> term frequency
-    Map<String,Double> tfQuery = new HashMap<String, Double>();     
+    Map<String,Double> tfQuery = new HashMap<String, Double>();
 
     /*
      * TODO : Your code here
@@ -66,29 +65,29 @@ public abstract class AScorer {
 
     for(String word : q.queryWords) {
     	//word = clean(word);
-    	
+
 		if(tfQuery.containsKey(word)) {
 			tfQuery.put(word, tfQuery.get(word) + 1.0);
 		} else {
 			tfQuery.put(word, 1.0);
 		}
     }
-    
+
     for(String word : tfQuery.keySet()) {
     	double idf_weight = 0.0;
-    	
+
     	if (this.idfs.containsKey(word))
     		idf_weight = this.idfs.get(word);
     	else
     		idf_weight = Math.log(1 + this.idfs.get(LoadHandler.no_word_in_doc_idf_val));
-    	
+
     	tfQuery.put(word, (1 + Math.log(tfQuery.get(word))) * idf_weight);
     }
-    
+
     return tfQuery;
   }
-  
-  
+
+
   /*
    * TODO : Your code here
    * Include any initialization and/or parsing methods
@@ -98,44 +97,45 @@ public abstract class AScorer {
    * the various fields are represented.
    */
 
-  
+
   /**
-   * Accumulate the various kinds of term frequencies 
+   * Accumulate the various kinds of term frequencies
    * for the fields (url, title, body, header, and anchor).
-   * You can override this if you'd like, but it's likely 
+   * You can override this if you'd like, but it's likely
    * that your concrete classes will share this implementation.
    * @param d the Document
    * @param q the Query
+ * @throws UnsupportedEncodingException
    */
-  public Map<String,Map<String, Double>> getDocTermFreqs(Document d, Query q) {
+  public Map<String,Map<String, Double>> getDocTermFreqs(Document d, Query q) throws UnsupportedEncodingException {
 
     // Map from tf type -> queryWord -> score
     Map<String,Map<String, Double>> tfs = new HashMap<String,Map<String, Double>>();
-    
+
     /*
      * TODO : Your code here
      * Initialize any variables needed
      */
-    
+
     //Isn't this going to be a problem if queries and idfs in general aren't lowercased?
     //So when we build the idf, we have to lowercase it as well right?
     for (String queryWord : q.queryWords) {
       /*
        * Your code here
-       * Loop through query terms and accumulate term frequencies. 
+       * Loop through query terms and accumulate term frequencies.
        * Note: you should do this for each type of term frequencies,
        * i.e. for each of the different fields.
        * Don't forget to lowercase the query word.
        */
-    	
+
     	// Is this sufficient?
     	queryWord = queryWord.toLowerCase();
-    	
+
     	String[] types = {"url","title","body","header","anchor"};
-    	
+
     	for (String type : types) {
     		Map<String, Double> type_map = new HashMap<String, Double>();
-    		
+
     		if (type.equals("url") && d.url != null) {
     			type_map.put(queryWord, (double)num_occurances_url(d.url, queryWord));
     		} else if (type.equals("title") && d.title != null) {
@@ -145,58 +145,62 @@ public abstract class AScorer {
 					type_map.put(queryWord, (double) d.body_hits.get(queryWord).size());
     		} else if (type.equalsIgnoreCase("header") && d.headers != null) {
     			int count = 0;
-    			
+
     			for(String header : d.headers)
-    				count += num_occurances(header, queryWord);    
-    			
+    				count += num_occurances(header, queryWord);
+
     			type_map.put(queryWord, (double)count);
     		} else if (type.equals("anchor") && d.anchors != null) {
     			int count = 0;
-    			
+
     			for(String anchor : d.anchors.keySet())
     				count += num_occurances(anchor, queryWord) * d.anchors.get(anchor);
-    			
+
 				type_map.put(queryWord, (double)count);
     		}
-    		
+
     		tfs.put(type, type_map);
     	}
-    	   	
+
     }
-    
+
     return tfs;
   }
 
   //how many times does shortStr occur in longStr
   private int num_occurances(String longStr, String shortStr) {
 	  longStr = longStr.toLowerCase();
-	  
+
 	  String[] words = longStr.split(" ");
 	  int count = 0;
-	  
+
 	  for(String word : words) {
 		  if (word.equals(shortStr)) {
 			  count++;
 		  }
 	  }
-	  
+
 	  return count;
   }
-  
-  private int num_occurances_url(String url, String shortStr) {
-	  url = url.toLowerCase();
-	  String[] words = url.split("[^A-Za-z0-9 ]");
-	  int count = 0;
-	  
-	  for(String word : words) {
-		  if (word.equals(shortStr)) {
-			  count++;
-		  }
-	  }
-	  
-	  return count;	  
+
+  private String[] urlSplit(String url) throws UnsupportedEncodingException {
+      String decoded = URLDecoder.decode(url, "UTF-8");
+      return decoded.split("[^A-Za-z0-9]+");
   }
-  
+
+  private int num_occurances_url(String url, String shortStr) throws UnsupportedEncodingException {
+      url = url.toLowerCase();
+      String[] words = urlSplit(url);
+      int count = 0;
+
+      for(String word : words) {
+          if (word.equals(shortStr)) {
+              count++;
+          }
+      }
+      return count;
+  }
+
   //gets rid of all non alphanumeric and makes all white space a single " " and trims off leading/ending whitespace
   public String clean(String str) {
 	  return str.toLowerCase().replaceAll("[^A-Za-z0-9 ]", "").replace("\\s+", " ").trim();
