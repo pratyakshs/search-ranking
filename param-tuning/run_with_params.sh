78e0d25bb169355f5225103938d4d92180733735
Pratyaksh Sharma <pratyaksh@me.com>
2017-05-17 23:38:02 -0700

    some scripts for tuning params

---
+++ param-tuning/run_with_params.sh
@@ -0,0 +1,5 @@
+#~/bin/bash
+
+cat cosinetemplate.java | sed "s/tune1/$1/g" | sed "s/tune2/$2/g" | sed "s/tune3/$3/g" | sed "s/tune4/$4/g" | sed "s/tune5/$5/g" > src/edu/stanford/cs276/CosineSimilarityScorer.java;
+ant &> /dev/null;
+./flow.sh pa3-data/pa3.signal.train cosine idfs false pa3-data/pa3.rel.train /tmp/cosine$1.txt
