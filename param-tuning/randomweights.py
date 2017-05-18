75caef468e8e71bb0ad8fc6132fd53cfeb22fe35
Pratyaksh Sharma <pratyaksh@me.com>
2017-05-18 01:44:09 -0700

    sample weights uniformly

--- param-tuning/randomweights.py
+++ param-tuning/randomweights.py
@@ -1,5 +1,11 @@
 #!/bin/python
 import random
-l = [random.random() for _ in range(5)]
-w = [r * 1.0 / sum(l) for r in l]
+
+def random_numbers(n):
+	l = [random.random() for _ in range(n-1)]
+	l = [0] + sorted(l) + [1]
+	l = [b-a for a, b in zip(l, l[1:])]
+	return l
+
+w = random_numbers(5)
 print w[0], w[1], w[2], w[3], w[4]
